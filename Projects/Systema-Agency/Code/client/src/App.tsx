2e4e738663973655277410def729c89988d5b138
Paw <paulineduluc07@gmail.com>
2026-03-20 13:59:49 -0400

    refactor: nettoyer App.tsx - retirer routes drawn-by-fate

--- Projects/Systema-Agency/Code/client/src/App.tsx
+++ Projects/Systema-Agency/Code/client/src/App.tsx
@@ -7,11 +7,6 @@ import { ThemeProvider } from "./contexts/ThemeContext";
 import { ConfigProvider } from "./contexts/ConfigContext";
 import Home from "./pages/Home";
 import { SuiviPage } from "./pages/Suivi";
-import DrawnByFateLanding from "./pages/drawn-by-fate/DrawnByFateLanding";
-import DrawnByFateReading from "./pages/drawn-by-fate/DrawnByFateReading";
-import DrawnByFateBook from "./pages/drawn-by-fate/DrawnByFateBook";
-import DrawnByFateGuide from "./pages/drawn-by-fate/DrawnByFateGuide";
-import DrawnByFateMonTirage from "./pages/drawn-by-fate/DrawnByFateMonTirage";
 import PromptVault from "./pages/PromptVault";
 
 function Router() {
@@ -19,11 +14,6 @@ function Router() {
     <Switch>
       <Route path={"/"} component={Home} />
       <Route path={"/suivi"} component={SuiviPage} />
-      <Route path={"/drawn-by-fate"} component={DrawnByFateLanding} />
-      <Route path={"/drawn-by-fate/reading"} component={DrawnByFateReading} />
-      <Route path={"/drawn-by-fate/book"} component={DrawnByFateBook} />
-      <Route path={"/drawn-by-fate/guide"} component={DrawnByFateGuide} />
-      <Route path={"/drawn-by-fate/mon-tirage"} component={DrawnByFateMonTirage} />
       <Route path={"/prompt-vault"} component={PromptVault} />
       <Route path={"/404"} component={NotFound} />
       <Route component={NotFound} />
